9e55cedebb6198fa7a2f5b2156b48ac9cd9b4b9f
Jake Atkin <jakeatkin@Jakes-MacBook-Air.local>
2019-02-05 14:53:26 +0000

    Added first test for dividing by three

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -4,6 +4,11 @@ describe('fizzbuzz',function(){
     fizzbuzz = new FizzBuzz();
   });
 
+    describe('divisble by 3', function(){
+      it('Checks to see if it generates fizz', function() {
+        expect(fizzbuzz.divisiblebythree(3)).toBe(true);
+      });
+    });
 
 
 
